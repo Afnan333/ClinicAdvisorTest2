9c5de070056d59936e9a3623df28032a6bbc355b
Afnan333 <oscar7-s@hotmail.com>
2018-02-19 21:02:56 +0300

    nwe update-

---
+++ app/src/main/java/com/example/se7en/clinicadvisortest2/clinic_info.java
@@ -0,0 +1,13 @@
+package com.example.se7en.clinicadvisortest2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class clinic_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinic_info);
+    }
+}
